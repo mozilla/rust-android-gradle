d6f70b381305ca299dbd2c62badd1fe0a67cb86f
Nick Alexander <nalexander@mozilla.com>
2018-09-17 11:14:39 -0700

    Require ANDROID_NDK_TOOLCHAIN_DIR.

--- plugin/src/main/kotlin/com/nishtahir/Extensions.kt
+++ plugin/src/main/kotlin/com/nishtahir/Extensions.kt
@@ -1,11 +1,10 @@
 package com.nishtahir
 
+import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.gradle.api.plugins.ExtensionContainer
 import java.io.File
 import kotlin.reflect.KClass
-
-const val TOOLS_FOLDER = ".cargo/toolchain"
 
 operator fun <T : Any> ExtensionContainer.get(type: KClass<T>): T = getByType(type.java)
 
@@ -13,9 +12,9 @@ fun Project.getToolchainDirectory(): File {
     // Share a single toolchain directory, if one is configured, but fall back to per-project
     // toolchains.
     val globalDir: String? = System.getenv("ANDROID_NDK_TOOLCHAIN_DIR")
-    if (globalDir != null) {
-        return File(globalDir).absoluteFile
-    } else {
-        return File(projectDir, TOOLS_FOLDER)
+    if (globalDir == null) {
+        val oldDefault = File(projectDir, ".cargo/toolchain")
+        throw GradleException("Must set ANDROID_NDK_TOOLCHAIN_DIR to use rust-android-gradle plugin (previously, defaulted to ${oldDefault.absolutePath}")
     }
+    return File(globalDir).absoluteFile
 }
