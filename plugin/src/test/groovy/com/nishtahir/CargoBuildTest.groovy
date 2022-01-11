537980a912afc2768fe5de46eaf5dcfb7b666a39
Nick Alexander <nalexander@mozilla.com>
2022-01-11 14:38:42 -0800

    Test that `targets = ...` produces per-target outputs.

--- plugin/src/test/groovy/com/nishtahir/CargoBuildTest.groovy
+++ plugin/src/test/groovy/com/nishtahir/CargoBuildTest.groovy
@@ -11,6 +11,48 @@ import com.nishtahir.Versions
 @MultiVersionTest
 class CargoBuildTest extends AbstractTest {
     @Unroll
+    def "cargoBuild produces #location for target #target"() {
+        given:
+        def androidVersion = TestVersions.latestAndroidVersionForCurrentJDK()
+
+        SimpleAndroidApp.builder(temporaryFolder.root, cacheDir)
+                .withAndroidVersion(androidVersion)
+                .withKotlinDisabled()
+        // TODO: .withCargo(...)
+                .build()
+                .writeProject()
+
+        SimpleCargoProject.builder(temporaryFolder.root)
+                .withTargets([target])
+                .build()
+                .writeProject()
+
+        when:
+        BuildResult buildResult = withGradleVersion(TestVersions.latestSupportedGradleVersionFor(androidVersion).version)
+                .withProjectDir(temporaryFolder.root)
+                .withArguments('cargoBuild', '--info', '--stacktrace')
+        // .withDebug(true)
+                .build()
+
+        // To ease debugging.
+        temporaryFolder.root.eachFileRecurse {
+            println(it)
+        }
+
+        then:
+        buildResult.task(':app:cargoBuild').outcome == TaskOutcome.SUCCESS
+        buildResult.task(':library:cargoBuild').outcome == TaskOutcome.SUCCESS
+        new File(temporaryFolder.root, "app/build/rustJniLibs/${location}").exists()
+        new File(temporaryFolder.root, "library/build/rustJniLibs/${location}").exists()
+
+        where:
+        [target, location] << [
+                ["darwin", "desktop/darwin/librust.dylib"],
+                ["linux-x86-64", "desktop/linux-x86-64/librust.so"],
+        ]
+    }
+
+    @Unroll
     def "cargoBuild is invoked with #gradleVersion and Android plugin #androidVersion"() {
         given:
         SimpleAndroidApp.builder(temporaryFolder.root, cacheDir)
