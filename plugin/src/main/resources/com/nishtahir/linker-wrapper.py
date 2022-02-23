8662eafc66754164868d2cd76b25a7b25506e49b
minyung <resone9@naver.com>
2022-02-23 22:49:43 +0900

    Change to using unwind library instead of gcc library.

--- plugin/src/main/resources/com/nishtahir/linker-wrapper.py
+++ plugin/src/main/resources/com/nishtahir/linker-wrapper.py
@@ -7,6 +7,13 @@ import sys
 
 args = [os.environ['RUST_ANDROID_GRADLE_CC'], os.environ['RUST_ANDROID_GRADLE_CC_LINK_ARG']] + sys.argv[1:]
 
+# The gcc library is not included starting from ndk version 23.
+ndk_major_version = os.environ['CARGO_NDK_MAJOR_VERSION']
+if ndk_major_version.isdigit():
+    if 23 <= int(ndk_major_version):
+        args.remove("-lgcc")
+        args.append("-lunwind")
+
 # This only appears when the subprocess call fails, but it's helpful then.
 printable_cmd = ' '.join(pipes.quote(arg) for arg in args)
 print(printable_cmd)
