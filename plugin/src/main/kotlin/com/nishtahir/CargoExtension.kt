e0a4141bfde1a00d2be591dc3ec519154acd2566
Ben Dean-Kawamura <bdk@mozilla.com>
2024-12-31 09:51:14 -0500

    Add --build-id to the linker flags

--- plugin/src/main/kotlin/com/nishtahir/CargoExtension.kt
+++ plugin/src/main/kotlin/com/nishtahir/CargoExtension.kt
@@ -44,6 +44,7 @@ open class CargoExtension {
     var apiLevel: Int? = null
     var apiLevels: Map<String, Int> = mapOf()
     var extraCargoBuildArguments: List<String>? = null
+    var generateBuildId: Boolean = false
 
     // It would be nice to use a receiver here, but there are problems interoperating with Groovy
     // and Kotlin that are just not worth working out.  Another JVM language, yet another dynamic
